1809d2d3a2b45eeb54ebf6600bd7f4b17733bc67
Benjamin Quiedeville <84285984+BenjaminQuiedeville@users.noreply.github.com>
2023-02-25 16:34:51 +0100

    Update Affichage.py

--- Affichage.py
+++ Affichage.py
@@ -2,15 +2,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def affichage(
-    frequences: np.ndarray,
-    color: np.ndarray, 
-    temps: np.ndarray, 
-    signalPreset: str, 
-    datatoplot: str,
-    critere: str,
-    save: bool
-    ) -> None:
+def affichage(frequences: np.ndarray,
+            color: np.ndarray, 
+            temps: np.ndarray, 
+            signalPreset: str, 
+            datatoplot: str,
+            critere: str,
+            save: bool
+            ) -> None:
     
     color[color == -200] = np.nan
     
@@ -22,7 +21,7 @@ def affichage(
     graph = ax.scatter(temps, frequences, s=25, c=color, cmap = "Reds")
     ax.set_ylim(ylimit)
     ax.set_xlim(0, temps[0, -1])
-    #ax.set_title(f"{datatoplot} - {signalPreset} - {critere}")
+    ax.set_title(f"{datatoplot} - {signalPreset} - {critere}")
     ax.set_xlabel("Temps (s)")
     ax.set_ylabel("Fréquence (Hz)")
 

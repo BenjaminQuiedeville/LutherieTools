ede5de4ec3681058323daf1a8f56a497ce894cd1
Benjamin Quiedeville <84285984+BenjaminQuiedeville@users.noreply.github.com>
2023-02-26 17:55:18 +0100

    Update Affichage.py

--- Affichage.py
+++ Affichage.py
@@ -13,7 +13,7 @@ def affichage(frequences: np.ndarray,
     
     color[color == -200] = np.nan
     
-    ylimit = (0, 3500)
+    ylimit = (0, 1000)
     
     fig, ax = plt.subplots(figsize = (8,6), tight_layout = True)
     
@@ -21,11 +21,12 @@ def affichage(frequences: np.ndarray,
     graph = ax.scatter(temps, frequences, s=25, c=color, cmap = "Reds")
     ax.set_ylim(ylimit)
     ax.set_xlim(0, temps[0, -1])
-    ax.set_title(f"{datatoplot} - {signalPreset} - {critere}")
+    #ax.set_title(f"{datatoplot} - {signalPreset} - {critere}")
+    ax.set_title(f"{datatoplot}")
     ax.set_xlabel("Temps (s)")
     ax.set_ylabel("Fréquence (Hz)")
 
     plt.colorbar(graph, label ="Amplitude (dB)")
     ax.grid(True)
 
-    if save: fig.savefig(f"/../{datatoplot}-{signalPreset}-{critere}.pdf")
+    if save: fig.savefig(f"{signalPreset}.pdf")

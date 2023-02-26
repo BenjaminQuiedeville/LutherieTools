e59651a4f8ac40ad583d87c0f148526c7a7d7728
Benjamin Quiedeville <84285984+BenjaminQuiedeville@users.noreply.github.com>
2023-02-26 17:54:49 +0100

    Create tracerGraph.py

---
+++ tracerGraph.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from HROgramme import HROgramme
+import Preset
+from Affichage import affichage
+
+
+
+listPreset = ["diapason", "cordeIdeale", "guitareSimulee", 
+              "guitareCorps", "guitareModesDoubles", "guitareBruit"]
+
+listTitres = ["Diapason", "Corde idéale", "Guitare Simulée", 
+              "Guitare avec modes de corps",
+              "Guitare avec modes de corde doubles", 
+              "Guitare avec bruit"]
+
+for signalPreset, titre in zip(listPreset, listTitres):
+
+    signal, params = Preset.preset("sample", "", signalPreset)
+
+    print(f"preset : {signalPreset}")
+    matrices = HROgramme(signal, params)
+    
+        
+    plt.close("all")
+    
+    affichage(matrices.F, matrices.BdBSeuil, matrices.T, signalPreset, 
+                titre, "", True)
+
+
+plt.show(block = True)

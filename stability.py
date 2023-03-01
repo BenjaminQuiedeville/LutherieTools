d82f798200dd86a77e3f700614c4059da6077b9c
Benjamin Quiedeville <quiedevilleb@gmail.com>
2023-03-01 18:16:57 +0100

    Update stability.py

--- stability.py
+++ stability.py
@@ -1,41 +1,43 @@
 import numpy as np
-from copy import deepcopy, copy
+from copy import copy
 
-def stability(inMatrix: np.ndarray, numcolstoverify: int, tolerancePourcent: float) -> np.ndarray:
+def stability(inMatrix: np.ndarray, numcolstoverify: int, 
+            tolerancePourcent: float) -> np.ndarray:
     
     modesValides = []
-    (numrows, numcols) = inMatrix.shape
+    numcols = inMatrix.shape[1]
     outMatrix = copy(inMatrix)
 
     for (rowIndex, colIndex), mode in np.ndenumerate(inMatrix):
 
-        tolerance: float = tolerancePourcent #* mode
+        tolerance = mode * tolerancePourcent
 
-        if mode in modesValides: 
-            continue 
+        # if mode in modesValides: 
+        #     continue 
         
-        elif ((numcols - colIndex) > numcolstoverify 
+        if ((numcols - colIndex) > numcolstoverify 
         and isValid(mode, inMatrix[:, (colIndex+1):numcolstoverify], tolerance)):
 
-            modesValides.append(mode)
+            # modesValides.append(mode)
+            continue
             
-        else:
+        else: 
             outMatrix[rowIndex, colIndex] = np.nan
 
     return outMatrix
 
 
-def isValid(value: float, colstoverify: np.ndarray, tolerance: float) -> bool:
+def isValid(value: float, arrayToVerify: np.ndarray, tolerance: float) -> bool:
 
-    numcols: int = colstoverify.shape[1]
-    numofvalidcols: int = 0
+    numcols: int = arrayToVerify.shape[1]
+    numOfValidCols: int = 0
 
-    for col in range(numcols):
+    for colIndex in range(numcols):
 
-        if isInInterval(value, colstoverify[:, col], tolerance):
-            numofvalidcols += 1
+        if isInInterval(value, arrayToVerify[:, colIndex], tolerance):
+            numOfValidCols += 1
 
-    return numofvalidcols == numcols
+    return numOfValidCols == numcols
 
     
 def isInInterval(value: float, vector: np.ndarray, tolerance: float) -> bool:

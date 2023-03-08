3ce0c14a099db91278ec6531a62ba968013e6ffd
Benjamin Quiedeville <quiedevilleb@gmail.com>
2023-03-08 11:48:24 +0100

    Simplification : suppression des boucles

--- stability.py
+++ stability.py
@@ -2,10 +2,7 @@ import numpy as np
 
 def stability(inMatrix: np.ndarray, numColsToVerify: int, 
               tolerancePourcent: float) -> np.ndarray:
-    """stability : parcourt la matrice des fréquences matrices.F pour vérifier la stabilité des modes utilisés. Pour chaque colonne et pour chaque mode, on cherche si le mode apparaît dans les {numColsToVerify} colonnes suivantes. Si le mode n'apparait pas dans toutes les colonnes vérifiées, on le remplace par np.NaN, sinon on le conserve.
-
-    isInIntervalle : 
-
+    """stability : parcourt la matrice des fréquences matrices.F pour vérifier la stabilité des modes utilisés. Pour chaque colonne et pour chaque mode, on cherche si le mode apparaît dans les {numColsToVerify} colonnes suivantes. Si le mode apparait dans toutes les colonnes vérifiées, on le garde, sinon on le remplace par np.NaN.
 
     Args:
         inMatrix (np.ndarray): matrices des Fréquences
@@ -17,32 +14,19 @@ def stability(inMatrix: np.ndarray, numColsToVerify: int,
     """
     def isValid(value: float, arrayToVerify: np.ndarray, 
                 tolerance: float) -> bool:
-        """isValid : compte le nombre de colonnes pour lesquelles isInInterval renvoie True, si le compte est égal au nombre de colonne à vérifier, renvoie True"""
-
-        return ((numcols := arrayToVerify.shape[1]) 
-            == np.sum([isInInterval(value, arrayToVerify[:, i], tolerance) 
-                      for i in range(numcols)]))
+        """isValid : compte le nombre de colonnes pour lesquelles np.isclose renvoie True, si le compte est égal au nombre de colonne à vérifier, renvoie True"""
+        return (arrayToVerify.shape[1] 
+                <= np.sum(np.isclose(value, arrayToVerify, 
+                                     atol = tolerance, rtol = 0.)))
     
-
-    def isInInterval(value: float, vector: np.ndarray, 
-                     tolerance: float) -> bool:
-        """Recherche si {value} apparait dans {vector} à {tolerance} près."""
-        return ((value >= vector - tolerance) 
-              & (value <= vector + tolerance)).any()
-
-
-    numcols = inMatrix.shape[1]
-    outMatrix = inMatrix.copy()
-
-    for (rowIndex, colIndex), mode in np.ndenumerate(inMatrix):
-
-        tolerance = mode * tolerancePourcent
-        
-        # on arrête la boucle avant la fin pour 
-        # éviter le dépacement hors de la matrice
-        if not ((numcols - colIndex) > numColsToVerify 
-            and isValid(mode, inMatrix[:, (colIndex+1):numColsToVerify], 
-                        tolerance)):
-            outMatrix[rowIndex, colIndex] = np.nan
-                        
-    return outMatrix
+    # on vérifie si on est pas à la fin de la matrice
+    # puis si le isValid renvoie True 
+    # sinon le mode devient np.NaN
+    # et ce pour tous les modes
+    return [inMatrix[row, col] 
+                if ((inMatrix.shape[1] - col) > numColsToVerify
+                and isValid(mode, 
+                            inMatrix[:, (col+1):(col+numColsToVerify+1)],
+                            mode * tolerancePourcent))
+                else np.NaN
+                for (row, col), mode in np.ndenumerate(inMatrix)]
